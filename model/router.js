c712eb6d6e5b5ee78875cd130b4b0698e83bf177
Dong Liu <liud@frib.msu.edu>
2014-09-29 16:50:47 -0400

    add route, hold to router model

--- model/router.js
+++ model/router.js
@@ -1,3 +1,5 @@
+/*jslint es5: true */
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
@@ -12,15 +14,82 @@ var sharedWithUser = new Schema({
   access: Number
 });
 
-// dependencies are the tasks that this task depends on.
-// This task cannot be started before the dependencies are finished.
 
-// var task = new Schema({
-//   sequence: Number,
-//   traveler: {type: ObjectId, ref: 'Traveler'},
-//   dependencies: [ObjectId],
-//   title: String
-// });
+var endorsement = new Schema({
+  endorsedBy: {
+    type: ObjectId,
+    ref: 'User'
+  },
+  endorsedOn: Date
+});
+
+
+var hold = new Schema({
+  title: String,
+  description: String,
+  createdBy: String,
+  createdOn: Date,
+  updatedBy: String,
+  updatedOn: Date,
+  stakeholders: [{
+    type: ObjectId,
+    ref: 'User'
+  }],
+  rule: {
+    type: String,
+    enum: {
+      values: ['one', 'majority', 'consensus'],
+      message: 'enum validator failed for "{PATH}" with value "{VALUE}"'
+    }
+  }
+});
+
+
+/*******
+status := 0 // initialized
+        | 1 // active
+        | 2 // passed
+*******/
+
+var holdPoint = new Schema({
+  title: String,
+  description: String,
+  status: Number,
+  referenceHold: {type: ObjectId, ref: 'Hold'},
+  createdBy: String,
+  createdOn: Date,
+  updatedBy: String,
+  updatedOn: Date,
+  stakeholders: [{
+    type: ObjectId,
+    ref: 'User'
+  }],
+  rule: {
+    type: String,
+    enum: {
+      values: ['one', 'majority', 'consensus'],
+      message: 'enum validator failed for "{PATH}" with value "{VALUE}"'
+    }
+  },
+  endorsements: [endorsement]
+});
+
+
+// route is the template for routers
+// a step is either a form or a hold
+var route = new Schema({
+  title: String,
+  description: String,
+  createdBy: String,
+  createdOn: Date,
+  updatedBy: String,
+  updatedOn: Date,
+  sharedWith: [sharedWithUser],
+  steps: [{
+    type: ObjectId,
+    ref: String
+  }]
+});
 
 
 /*******
@@ -30,23 +99,57 @@ status := 0 // initialized
         | 3 // aborted
 *******/
 
+// task is either a traveler or a hold point
 var router = new Schema({
   title: String,
   description: String,
+  device: String,
   status: Number,
   createdBy: String,
   createdOn: Date,
   updatedBy: String,
   updatedOn: Date,
   sharedWith: [sharedWithUser],
-  tasks: [{type: ObjectId, ref: String}],
-  finishedTasks: [{type: ObjectId, ref: String}],
-  referenceRouter: {type: ObjectId, ref: 'Router'}
+  tasks: [{
+    type: ObjectId,
+    ref: String
+  }],
+  finishedTasks: [{
+    type: ObjectId,
+    ref: String
+  }],
+  notes: [{
+    type: ObjectId,
+    ref: 'RouterNote'
+  }],
+  referenceRouter: {
+    type: ObjectId,
+    ref: 'Router'
+  }
+});
+
+// task is either a traveler or a hold point
+var taskNote = new Schema({
+  task: {
+    type: ObjectId,
+    ref: String
+  },
+  value: String,
+  inputBy: String,
+  inputOn: Date
 });
 
 
+var Hold = mongoose.model('Hold', hold);
+var HoldPoint = mongoose.model('HoldPoint', holdPoint);
+var Route = mongoose.model('Route', route);
 var Router = mongoose.model('Router', router);
+var TaskNote = mongoose.model('TaskNote', taskNote);
 
 module.exports = {
-  Router: Router
+  Hold: Hold,
+  HoldPoint: HoldPoint,
+  Route: Route,
+  Router: Router,
+  TaskNote: TaskNote
 };
